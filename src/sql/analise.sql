9ab5bd3020eaecb41dfef9ad7687d1fa967a2955
Gabriel Cordeiro <cordeirodados@gmail.com>
2023-01-20 14:27:38 -0300

    Criando Analise Inicial

--- src/sql/analise.sql
+++ src/sql/analise.sql
@@ -3,7 +3,8 @@ SELECT
     SUM(T2.price) AS RECEITA_TOTAL,
     COUNT(DISTINCT T1.order_id) AS QTD_PEDIDOS,
     COUNT( T2.product_id) AS QTD_PRODUTOS,
-    COUNT(DISTINCT T2.product_id) AS QTD_DISTINTA
+    COUNT(DISTINCT T2.product_id) AS QTD_DISTINTA,
+    MIN( DATE('2018-06-01') - T1.order_approved_at) AS QTD_DIAS_ULT_VENDA
 FROM tb_orders AS T1
 
 LEFT JOIN tb_order_items AS T2 
